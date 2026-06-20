8b84a6d86d533a6178ba0f5219024e242143ae11
Sejal Kshirsagar <sejalsksagar@gmail.com>
2026-06-20 22:26:59 +0530

    update README

--- backend/scripts/decode_route.py
+++ backend/scripts/decode_route.py
@@ -2,7 +2,7 @@ import requests
 import polyline
 import json
 
-url = "https://route.mappls.com/route/direction/route_adv/driving/77.5610,12.9780;77.4800,12.9880?access_token=lknudnksjmmxaqvhydjhzqpknzdjndrmmoev"
+url = "https://route.mappls.com/route/direction/route_adv/driving/77.5610,12.9780;77.4800,12.9880?access_token=<API_KEY>"
 
 data = requests.get(url).json()
 
